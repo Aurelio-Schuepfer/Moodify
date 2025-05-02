87522b563fe68a78737f8b5c004f39378f9958f0
Aurelio-Schuepfer <aurelio.schuepfer@dialog.ch>
2025-05-02 09:49:48 +0200

    Moodify 1.0 - Base Design and Functionality
    -Design Based on Mood
    
    -Base Layout
    
    -Songs recommended based on Mood
    
    Muliple Moods

---
+++ Backend/MoodifyAPI/Controllers/MoodController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MoodifyAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoodController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetMoods()
+        {
+            var moods = new string[] { "Happy", "Sad", "Excited", "Angry" };
+            return Ok(moods);
+        }
+    }
+}
